99a3995ac9c526b0679331911f43139a8b35bd10
NishaPAliyarcal <cal.nishap@gmail.com>
2023-06-07 14:29:21 +0530

    mysingleinheritance project.

---
+++ mavensingleinheritance/src/main/java/mavensingleinheritancepackage/msingleinheritance.java
@@ -0,0 +1,8 @@
+package mavensingleinheritancepackage;
+
+public class msingleinheritance {
+public void view()
+{
+	System.out.println("first class");
+}
+}
---
+++ mavensingleinheritance/src/main/java/mavensingleinheritancepackage/msingleinheritance2.java
@@ -0,0 +1,17 @@
+package mavensingleinheritancepackage;
+
+public class msingleinheritance2 extends msingleinheritance {
+public void view2()
+{
+	System.out.println("Second class");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		msingleinheritance2 m2 = new msingleinheritance2();
+		m2.view2();
+		m2.view();
+		
+	
+	}
+
+}
