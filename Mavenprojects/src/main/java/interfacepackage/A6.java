35372c897e56fc010e8b7f0000466de3c29cdb72
NishaPAliyarcal <cal.nishap@gmail.com>
2023-06-09 14:35:29 +0530

    Interface program completed.

---
+++ Mavenprojects/src/main/java/interfacepackage/A6.java
@@ -0,0 +1,14 @@
+package interfacepackage;
+
+public class A6 implements Printable{
+public void print()
+{
+	System.out.println("Hello");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  A6 obj = new A6();
+  obj.print();
+	}
+
+}
---
+++ Mavenprojects/src/main/java/interfacepackage/Printable.java
@@ -0,0 +1,5 @@
+package interfacepackage;
+
+public interface Printable {
+void print();
+}
